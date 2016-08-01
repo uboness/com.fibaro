6c37a8c2c161bf84fbbf40aa48cae1cf80c1e18a
Emile Nijssen <emile@athom.nl>
2016-08-01 18:05:04 +0200

    add measure_temperature to FGSS-001

--- drivers/FGSS-001/driver.js
+++ drivers/FGSS-001/driver.js
@@ -55,6 +55,23 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 				return report['Sensor State'] === 'alarm';
 			}
 		},
+		'measure_temperature': {
+			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get'				: 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser'		: function(){
+				return {
+					'Sensor Type': 'Temperature (version 1)'
+				}
+			},
+			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
+			'command_report_parser'		: function( report ) {
+
+				if( report['Sensor Type'] !== 'Temperature (version 1)' )
+					return null;
+
+				return report['Sensor Value (Parsed)'];
+			}
+		},
 		'measure_battery': {
 			'command_class'				: 'COMMAND_CLASS_BATTERY',
 			'command_get'				: 'BATTERY_GET',
