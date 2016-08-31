ff7076c9a631d6d3c40d97dcbb5a63b5a3eb52c5
Remco <caseda08@hotmail.com>
2016-08-31 22:39:07 +0200

    FGWPE-101 update
    Add Actions:
    Led Ring (On State)
    Led Ring (Off State)
    
    Add meter_power capability
    
    Add rest of usefull parameters

--- drivers/FGWPE-101/driver.js
+++ drivers/FGWPE-101/driver.js
@@ -28,6 +28,20 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report_parser'		: function( report ){
 				return report['Sensor Value (Parsed)'];
 			}
+		},
+		'meter_power': {
+			'command_class'				: 'COMMAND_CLASS_METER',
+			'command_get'				: 'METER_GET',
+			'command_get_parser': function() {
+				return {
+					'Properties1': {
+						'Meter Type': 1,
+						'Scale': 0
+					}
+				};
+			},
+			'command_report'			: 'METER_REPORT',
+			'command_report_parser'		: report => report['Meter Value (Parsed)']
 		}
 	},
 	settings: {
@@ -35,7 +49,33 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			"index": 1,
 			"size": 1,
 			"parser": function( value ){
-				return new Buffer([ ( value === true ) ? 0 : 1 ]);
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		},
+		"save_state": {
+			"index": 16,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		},
+		"power_report": {
+			"index": 40,
+			"size": 1
+		},
+		"power_load_report": {
+			"index": 42,
+			"size": 1
+		},
+		"power_report_interval": {
+			"index": 43,
+			"size": 1
+		},
+		"own_power": {
+			"index": 49,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
 			}
 		},
 		"led_ring_color_on": {
@@ -47,8 +87,36 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			"size": 1
 		}
 	}
-})
+});
 
 module.exports.on('applicationUpdate', function( device_data, buf ){
 	Homey.manager('flow').triggerDevice( 'fgwpe-101_nif', null, null, device_data )
+});
+
+Homey.manager('flow').on('action.FGWPE_led_on', function( callback, args ) {
+	var node = module.exports.nodes[ args.device['token'] ];
+	var color = new Buffer([ args.color ]);
+	node.instance.CommandClass['COMMAND_CLASS_CONFIGURATION'].CONFIGURATION_SET( {
+		"Parameter Number": 61,
+		"Level": {
+			"Size": 1,
+			"Default": false
+		},
+		'Configuration Value': color
+	});
+	callback(null, true);
+});
+
+Homey.manager('flow').on('action.FGWPE_led_off', function( callback, args ) {
+	var node = module.exports.nodes[ args.device['token'] ];
+	var color = new Buffer([ args.color ]);
+	node.instance.CommandClass['COMMAND_CLASS_CONFIGURATION'].CONFIGURATION_SET( {
+		"Parameter Number": 62,
+		"Level": {
+			"Size": 1,
+			"Default": false
+		},
+		'Configuration Value': color
+	});
+	callback(null, true);
 });
