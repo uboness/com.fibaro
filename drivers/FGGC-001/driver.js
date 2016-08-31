b6ef5e1eafc2826fb2402cc690de181312de25be
Remco <caseda08@hotmail.com>
2016-08-31 23:00:15 +0200

    Update FGGC-001
    Added all default gestures:
    Single, Double Swipe for Up, Down, Left and Right swipes.
    Continue Circle, Stop Circle for Clockwise and Counter Clockwise
    
    Added most common parameters

--- drivers/FGGC-001/driver.js
+++ drivers/FGGC-001/driver.js
@@ -7,36 +7,64 @@ const ZwaveDriver	= require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
 	capabilities: {
+	},
+	settings: {
+		"device_orientation": {
+			"index": 1,
+			"size": 1
+		},
+		"acoustic_signal": {
+			"index": 2,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		},
+		"visual_signal": {
+			"index": 3,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		},
+		"buzzer_behaviour": {
+			"index": 4,
+			"size": 1
+		}
 	}
-})
+});
 
-// bind Flow
 module.exports.on('initNode', function( token ){
-
 	var node = module.exports.nodes[ token ];
 	if( node ) {
 		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', function( command, report ){
 			if( command.name === 'CENTRAL_SCENE_NOTIFICATION' ) {
-
-				var trigger = 'fggc-001_';
-
-				switch( report['Scene Number'] ) {
-					case 1:
-						trigger += 'up';
-						break;
-					case 2:
-						trigger += 'down';
-						break;
-					case 3:
-						trigger += 'left';
-						break;
-					case 4:
-						trigger += 'right';
-						break;
+				var swiped = {
+					"direction": report['Scene Number'].toString(),
+					"scene": report.Properties1['Key Attributes']
+				};
+				if(report['Scene Number'] >= 1 && report['Scene Number'] <= 4) {
+					Homey.manager('flow').triggerDevice('fggc-001_swipe_direction', null, swiped, node.device_data);
+				} else {
+					Homey.manager('flow').triggerDevice('fggc-001_swipe_round', null, swiped, node.device_data);
 				}
-
-				Homey.manager('flow').triggerDevice(trigger, null, null, node.device_data);
 			}
 		});
 	}
-})
+});
+
+Homey.manager('flow').on('trigger.fggc-001_swipe_direction', function( callback, args, state ) {
+	if(state.direction === args.direction && state.scene === args.scene) {
+		callback(null, true);
+		return;
+	}
+	callback(null, false);
+});
+
+Homey.manager('flow').on('trigger.fggc-001_swipe_round', function( callback, args, state ) {
+	if(state.direction === args.direction && state.scene === args.scene) {
+		callback(null, true);
+		return;
+	}
+	callback(null, false);
+});
