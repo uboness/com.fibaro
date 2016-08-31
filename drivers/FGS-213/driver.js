f89fcd907346e69223a60b9d612f09521caee357
Remco <caseda08@hotmail.com>
2016-08-31 22:50:11 +0200

    add support: FGS-213 & FGS-223

---
+++ drivers/FGS-213/driver.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const path			= require('path');
+const ZwaveDriver	= require('homey-zwavedriver');
+
+// http://manuals.fibaro.com/content/manuals/en/FGS-2x3/FGS-2x3-EN-T-v1.0.pdf
+// FGS-213
+
+module.exports = new ZwaveDriver( path.basename(__dirname), {
+	capabilities: {
+		'onoff': {
+			'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
+			'command_get': 'SWITCH_BINARY_GET',
+			'command_set': 'SWITCH_BINARY_SET',
+			'command_set_parser': value => {
+				return {
+					'Switch Value': ( value > 0 ) ? 3 : 0
+				};
+			},
+			'command_report': 'SWITCH_BINARY_REPORT',
+			'command_report_parser': report => {
+				return report['Value'] === 'on/enable';
+			}
+		},
+		
+		'measure_power': {
+			'command_class': 'COMMAND_CLASS_METER',
+			'command_get': 'METER_GET',
+			'command_get_parser': function() {
+				return {
+					'Meter Type': 2,
+					'Properties1': {
+						'Scale': 0
+					}
+				};
+			},
+			'command_report': 'METER_REPORT',
+			'command_report_parser': report => {
+				if(report['Meter Type'] === 2) {
+					return report['Meter Value (Parsed)'];
+				}
+			}
+		},
+		
+		'meter_power': {
+			'command_class': 'COMMAND_CLASS_METER',
+			'command_get': 'METER_GET',
+			'command_get_parser': function() {
+				return {
+					'Meter Type': 0,
+					'Properties1': {
+						'Scale': 0
+					}
+				};
+			},
+			'command_report': 'METER_REPORT',
+			'command_report_parser': report => {
+				if(report['Meter Type'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+			}
+		}
+	},
+	settings: {
+		"save_state": {
+			"index": 9,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		},
+		"switch_type": {
+			"index": 20,
+			"size": 1
+		},
+		"s1_power_report": {
+			"index": 50,
+			"size": 1
+		},
+		"s1_power_report_interval": {
+			"index": 51,
+			"size": 1
+		},
+		"s1_energie_report": {
+			"index": 53,
+			"size": 2
+		},
+		"power_report_interval": {
+			"index": 58,
+			"size": 2
+		},
+		"energie_report_interval": {
+			"index": 59,
+			"size": 2
+		},
+		"own_power": {
+			"index": 60,
+			"size": 1,
+			"parser": function( value ){
+				return new Buffer([ ( value === true ) ? 1 : 0 ]);
+			}
+		}
+	}
+});
