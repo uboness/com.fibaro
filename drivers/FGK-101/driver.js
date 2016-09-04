8fc57741a01a6f7d504d256f1318a54a1b152430
Remco <caseda08@hotmail.com>
2016-09-04 23:37:35 +0200

    change alarm contact class FGK-101
    Change the command class for the alarm contact to BASIC
    Sensor Binary is not working (properly)

--- drivers/FGK-101/driver.js
+++ drivers/FGK-101/driver.js
@@ -8,16 +8,11 @@ const ZwaveDriver	= require('homey-zwavedriver');
 module.exports = new ZwaveDriver( path.basename(__dirname), {
 	capabilities: {
 		'alarm_contact': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_BINARY',
-			'command_get'				: 'SENSOR_BINARY_GET',
-			'command_get_parser'		: function() {
-				return {
-					'Sensor Type': 'Door/Window'
-				};
-			},
-			'command_report'			: 'SENSOR_BINARY_REPORT',
+			'command_class'				: 'COMMAND_CLASS_BASIC',
+			'command_get'				: 'BASIC_GET',
+			'command_report'			: 'BASIC_REPORT',
 			'command_report_parser'		: function( report ){
-				return report['Sensor Value'] === 'detected an event';
+				return report['Value'] > 0;
 			}
 		},
 		'measure_battery': {
