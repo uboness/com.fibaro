9bc6363cb1a5d1fa41768e608dc47674ce64cd7a
Remco <caseda08@hotmail.com>
2016-09-24 01:50:05 +0200

    Update FGFS-101 (PLUS)
    FGFS-101:
    Coding consistency (ESlint)
    Removed unnecessary checkbox parser
    Removed tamper alarm settings and driver capability.
    (tamper alarm not working properly in this version anyway)
    
    FGFS-101-PLUS
    Coding consistency (ESlint)
    Removed unnecessary checkbox parser

--- drivers/FGFS-101/driver.js
+++ drivers/FGFS-101/driver.js
@@ -10,32 +10,14 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		'alarm_water': {
 			'command_class': 'COMMAND_CLASS_SENSOR_ALARM',
 			'command_get': 'SENSOR_ALARM_GET',
-			'command_get_parser': function(){
+			'command_get_parser': () => {
 				return {
 					'Sensor Type': 'Water Leak Alarm'
-				}
+				};
 			},
 			'command_report': 'SENSOR_ALARM_REPORT',
-			'command_report_parser': function( report ){
-				if( report['Sensor Type'] !== 'Water Leak Alarm' )
-					return null;
-
-				return report['Sensor State'] === 'alarm';
-			}
-		},
-
-		'alarm_tamper': {
-			'command_class': 'COMMAND_CLASS_SENSOR_ALARM',
-			'command_get': 'SENSOR_ALARM_GET',
-			'command_get_parser': function(){
-				return {
-					'Sensor Type': 'General Purpose Alarm'
-				}
-			},
-			'command_report': 'SENSOR_ALARM_REPORT',
-			'command_report_parser': function( report ){
-				if( report['Sensor Type'] !== 'General Purpose Alarm' )
-					return null;
+			'command_report_parser': report => {
+				if (report['Sensor Type'] !== 'Water Leak Alarm') return null;
 
 				return report['Sensor State'] === 'alarm';
 			}
@@ -45,7 +27,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'multiChannelNodeId': 2,
 			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
 			'command_get': 'SENSOR_MULTILEVEL_GET',
-			'command_get_parser': function(){
+			'command_get_parser': () => {
 				return {
 					'Sensor Type': 'Temperature (version 1)',
 					'Properties1': {
@@ -54,20 +36,20 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 				}
 			},
 			'command_report': 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser': function( report ){
-				if( report['Sensor Type'] !== 'Temperature (version 1)' ) {
-					return null;
-				}
+			'command_report_parser': report => {
+				if (report['Sensor Type'] !== 'Temperature (version 1)') return null;
 
 				return report['Sensor Value (Parsed)'];
 			}
 		},
+		
 		'measure_battery': {
 			'command_class': 'COMMAND_CLASS_BATTERY',
 			'command_get': 'BATTERY_GET',
 			'command_report': 'BATTERY_REPORT',
-			'command_report_parser': function( report ) {
-				if( report['Battery Level'] === "battery low warning" ) return 1;
+			'command_report_parser': report => {
+				if (report['Battery Level'] === "battery low warning") return 1;
+				
 				return report['Battery Level (Raw)'][0];
 			},
 			'optional': true
@@ -76,58 +58,52 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 	settings: {
 		"alarm_cancellation": {
 			"index": 1,
-			"size": 2
+			"size": 2,
 		},
 		"flood_signal": {
 			"index": 2,
-			"size": 1
+			"size": 1,
 		},
 		"alarm_cancel_status": {
 			"index": 9,
 			"size": 1,
-			"parser": function( value ){
-				return new Buffer([ ( value === true ) ? 1 : 0 ]);
 			}
 		},
 		"temperature_measure_interval": {
 			"index": 10,
-			"size": 2
+			"size": 2,
 		},
 		"temperature_measure_hysteresis": {
 			"index": 12,
-			"size": 2
+			"size": 2,
 		},
 		"low_temperature_treshold": {
 			"index": 50,
-			"size": 2
+			"size": 2,
 		},
 		"high_temperature_treshold": {
 			"index": 51,
-			"size": 2
+			"size": 2,
 		},
 		"low_temperature_led": {
 			"index": 61,
-			"size": 4
+			"size": 4,
 		},
 		"high_temperature_led": {
 			"index": 62,
-			"size": 4
+			"size": 4,
 		},
 		"led_indication": {
 			"index": 63,
-			"size": 1
+			"size": 1,
 		},
 		"temperature_offset": {
 			"index": 73,
-			"size": 2
-		},
-		"tamper_alarm": {
-			"index": 74,
-			"size": 1
+			"size": 2,
 		},
 		"alarm_duration": {
 			"index": 75,
-			"size": 2
+			"size": 2,
 		},
 		"flood_sensor": {
 			"index": 77,
