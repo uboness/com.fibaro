4385ade1c07a1730f6a9807c745638d8600d76e2
Remco <caseda08@hotmail.com>
2016-09-25 23:45:03 +0200

    Fix & Update FGSS-001 & FGSD-002
    Coding consistency (ESlint)
    Using the proper alarm state for reporting

--- drivers/FGSD-002/driver.js
+++ drivers/FGSD-002/driver.js
@@ -1,71 +1,71 @@
-"use strict";
+'use strict';
 
-const path			= require('path');
-const ZwaveDriver	= require('homey-zwavedriver');
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
 
 // http://www.pepper1.net/zwavedb/device/675
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
 	capabilities: {
 		'alarm_smoke': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_ALARM',
-			'command_get'				: 'SENSOR_ALARM_GET',
-			'command_get_parser'		: function(){
+			'command_class': 'COMMAND_CLASS_SENSOR_ALARM',
+			'command_get': 'SENSOR_ALARM_GET',
+			'command_get_parser': () => {
 				return {
 					'Sensor Type': 'Smoke Alarm'
-				}
+				};
 			},
-			'command_report'			: 'SENSOR_ALARM_REPORT',
-			'command_report_parser'		: function( report ){
-				if( report['Sensor Type'] !== 'Smoke Alarm' )
-					return null;
+			'command_report': 'SENSOR_ALARM_REPORT',
+			'command_report_parser': report => {
+				if (report['Sensor Type'] !== 'Smoke Alarm') return null;
 
-				return report['Sensor Value'] === 'detected an event';
+				return report['Sensor State'] === 'alarm';
 			}
 		},
+		
 		'alarm_heat': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_ALARM',
-			'command_get'				: 'SENSOR_ALARM_GET',
-			'command_get_parser'		: function(){
+			'command_class': 'COMMAND_CLASS_SENSOR_ALARM',
+			'command_get': 'SENSOR_ALARM_GET',
+			'command_get_parser': () => {
 				return {
 					'Sensor Type': 'Heat Alarm'
-				}
+				};
 			},
-			'command_report'			: 'SENSOR_ALARM_REPORT',
-			'command_report_parser'		: function( report ){
-				if( report['Sensor Type'] !== 'Heat Alarm' )
-					return null;
+			'command_report': 'SENSOR_ALARM_REPORT',
+			'command_report_parser': report => {
+				if (report['Sensor Type'] !== 'Heat Alarm') return null;
 
-				return report['Sensor Value'] === 'detected an event';
+				return report['Sensor State'] === 'alarm';
 			}
 		},
+		
 		'measure_temperature': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			'command_get'				: 'SENSOR_MULTILEVEL_GET',
-			'command_get_parser'		: function(){
+			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get': 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser': () => {
 				return {
 					'Sensor Type': 'Temperature (version 1)',
 					'Properties1': {
 						'Scale': 0
 					}
-				}
+				};
 			},
-			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser'		: function( report ) {
-
-				if( report['Sensor Type'] !== 'Temperature (version 1)' )
-					return null;
+			'command_report': 'SENSOR_MULTILEVEL_REPORT',
+			'command_report_parser': report => {
+				if (report['Sensor Type'] !== 'Temperature (version 1)') return null;
 
 				return report['Sensor Value (Parsed)'];
 
 			}
 		},
+		
 		'measure_battery': {
-			'command_class'				: 'COMMAND_CLASS_BATTERY',
-			'command_get'				: 'BATTERY_GET',
-			'command_report'			: 'BATTERY_REPORT',
-			'command_report_parser'		: function( report ) {
+			'command_class': 'COMMAND_CLASS_BATTERY',
+			'command_get': 'BATTERY_GET',
+			'command_report': 'BATTERY_REPORT',
+			'command_report_parser': report => {
 				if( report['Battery Level'] === "battery low warning" ) return 1;
+				
 				return report['Battery Level (Raw)'][0];
 			}
 		}
@@ -73,27 +73,27 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 	settings: {
 		"smoke_sensitivity": {
 			"index": 1,
-			"size": 1
+			"size": 1,
 		},
 		"visual_notification": {
 			"index": 3,
-			"size": 1
+			"size": 1,
 		},
 		"acoustic_notification": {
 			"index": 4,
-			"size": 1
+			"size": 1,
 		},
 		"temperature_report_interval": {
 			"index": 20,
-			"size": 2
+			"size": 2,
 		},
 		"temperature_report_hysteresis": {
 			"index": 21,
-			"size": 1
+			"size": 1,
 		},
 		"temperature_alarm_treshold": {
 			"index": 30,
-			"size": 1
-		}
+			"size": 1,
+		},
 	}
 });
