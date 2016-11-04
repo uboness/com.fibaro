867df11d798944eba37f08a71b66cda18f0664d9
Robin Bolscher <robinbolscher@gmail.com>
2016-11-04 15:31:17 +0100

    FGS-213: fix meter_power and measure_power report parsers

--- drivers/FGS-213/driver.js
+++ drivers/FGS-213/driver.js
@@ -20,7 +20,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report': 'SWITCH_BINARY_REPORT',
 			'command_report_parser': report => report['Value'] === 'on/enable'
 		},
-		
+
 		'measure_power': {
 			'command_class': 'COMMAND_CLASS_METER',
 			'command_get': 'METER_GET',
@@ -33,15 +33,15 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			},
 			'command_report': 'METER_REPORT',
 			'command_report_parser': report => {
-				if (report.hasOwnProperty('Properties1') &&
-				report.Properties1.hasOwnProperty('Scale') &&
-				report.Properties1['Scale'] === 2)
-					return report['Meter Value (Parsed)'];
-					
-				return null;
+				if (report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale bits 10') &&
+					report.Properties2['Scale bits 10'] === 0)
+					return null;
+
+				return report['Meter Value (Parsed)'];
 			}
 		},
-		
+
 		'meter_power': {
 			'command_class': 'COMMAND_CLASS_METER',
 			'command_get': 'METER_GET',
@@ -54,12 +54,12 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			},
 			'command_report': 'METER_REPORT',
 			'command_report_parser': report => {
-				if (report.hasOwnProperty('Properties1') &&
-				report.Properties1.hasOwnProperty('Scale') &&
-				report.Properties1['Scale'] === 0)
-					return report['Meter Value (Parsed)'];
-				
-				return null;
+				if (report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale bits 10') &&
+					report.Properties2['Scale bits 10'] === 2)
+					return null;
+
+				return report['Meter Value (Parsed)'];
 			}
 		}
 	},
