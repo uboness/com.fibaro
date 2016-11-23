815615887e9d2287af11a8a902931d888d893134
Robin Bolscher <robinbolscher@gmail.com>
2016-11-23 20:12:45 +0100

    FGMS-001: re-add removed command_get's

--- drivers/FGMS-001-PLUS/driver.js
+++ drivers/FGMS-001-PLUS/driver.js
@@ -5,25 +5,33 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 // http://manuals.fibaro.com/content/manuals/en/FGMS-001/FGMS-001-EN-T-v2.0.pdf
 
-module.exports = new ZwaveDriver( path.basename(__dirname), {
+module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
 		'alarm_motion': {
 			'command_class': 'COMMAND_CLASS_NOTIFICATION',
+			'command_get': 'NOTIFICATION_GET',
+			'command_get_parser': () => {
+				return {
+					'V1 Alarm Type': 0,
+					'Notification Type': 'Home Security',
+					'Event': 7
+				};
+			},
 			'command_report': 'NOTIFICATION_REPORT',
 			'command_report_parser': report => {
 				if (report['Notification Type'] === 'Home Security' &&
-				report.hasOwnProperty("Event")) {
-					
+					report.hasOwnProperty("Event")) {
+
 					if (report['Event (Parsed)'] === 'Motion Detection' ||
-					report['Event (Parsed)'] === 'Motion Detection, Unknown Location')
+						report['Event (Parsed)'] === 'Motion Detection, Unknown Location')
 						return true;
-					
+
 					if (report['Event (Parsed)'] === 'Event inactive' &&
-					report.hasOwnProperty("Event Parameter") &&
-					(report['Event Parameter'][0] === 7 ||
-					report['Event Parameter'][0] === 8))
+						report.hasOwnProperty("Event Parameter") &&
+						(report['Event Parameter'][0] === 7 ||
+						report['Event Parameter'][0] === 8))
 						return false;
-				
+
 					return null;
 				}
 			}
@@ -31,19 +39,27 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 
 		'alarm_tamper': {
 			'command_class': 'COMMAND_CLASS_NOTIFICATION',
+			'command_get': 'NOTIFICATION_GET',
+			'command_get_parser': () => {
+				return {
+					'V1 Alarm Type': 0,
+					'Notification Type': 'Home Security',
+					'Event': 3
+				};
+			},
 			'command_report': 'NOTIFICATION_REPORT',
 			'command_report_parser': report => {
 				if (report['Notification Type'] === 'Home Security' &&
-				report.hasOwnProperty("Event")) {
-				
+					report.hasOwnProperty("Event")) {
+
 					if (report['Event (Parsed)'] === 'Tampering, Product covering removed')
 						return true;
-					
+
 					if (report['Event (Parsed)'] === 'Event inactive' &&
-					report.hasOwnProperty("Event Parameter") &&
-					report['Event Parameter'][0] === 3)
+						report.hasOwnProperty("Event Parameter") &&
+						report['Event Parameter'][0] === 3)
 						return false;
-					
+
 					return null;
 				}
 			}
@@ -64,7 +80,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report_parser': report => {
 				if (report['Sensor Type'] === 'Temperature (version 1)')
 					return report['Sensor Value (Parsed)'];
-					
+
 				return null;
 			}
 		},
@@ -84,7 +100,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report_parser': report => {
 				if (report['Sensor Type'] === 'Luminance (version 1)')
 					return report['Sensor Value (Parsed)'];
-					
+
 				return null;
 			}
 		},
@@ -96,7 +112,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report_parser': report => {
 				if (report['Battery Level'] === "battery low warning")
 					return 1;
-				
+
 				return report['Battery Level (Raw)'][0];
 			}
 		}
