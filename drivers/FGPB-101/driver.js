84b53932c8cedd16722998542fef648e46928363
Robin Bolscher <robinbolscher@gmail.com>
2017-01-04 18:08:11 +0100

    Textual fix, code format

--- drivers/FGPB-101/driver.js
+++ drivers/FGPB-101/driver.js
@@ -5,7 +5,7 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 // http://manuals.fibaro.com/content/manuals/en/FGPB-101/FGPB-101-EN-T-v1.0.pdf
 
-module.exports = new ZwaveDriver( path.basename(__dirname), {
+module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
 		'measure_battery': {
 			'command_class': 'COMMAND_CLASS_BATTERY',
@@ -13,36 +13,36 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report': 'BATTERY_REPORT',
 			'command_report_parser': report => {
 				if (report['Battery Level'] === "battery low warning") return 1;
-				
+
 				return report['Battery Level (Raw)'][0];
 			}
 		}
 	}
 });
 
-module.exports.on('initNode', function (token){
+module.exports.on('initNode', function (token) {
 	const node = module.exports.nodes[token];
 	let debouncer = 0;
-	
+
 	if (node) {
 		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', (command, report) => {
 			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
-				
+
 				if (report &&
-				report.hasOwnProperty('Properties1') &&
-				report.Properties1.hasOwnProperty('Key Attributes')) {
-					
+					report.hasOwnProperty('Properties1') &&
+					report.Properties1.hasOwnProperty('Key Attributes')) {
+
 					const button_value = { "scene": report.Properties1['Key Attributes'] };
-					
+
 					if (report.Properties1['Key Attributes'] === 'Key Released') {
 						if (debouncer === 0) {
 							Homey.manager('flow').triggerDevice('FGPB-101', null, button_value, node.device_data);
-							
+
 							debouncer++;
 							setTimeout(() => debouncer = 0, 2000);
 						}
 					}
-					
+
 					else {
 						Homey.manager('flow').triggerDevice('FGPB-101', null, button_value, node.device_data);
 					}
@@ -58,7 +58,7 @@ Homey.manager('flow').on('trigger.FGPB-101', (callback, args, state) => {
 		args &&
 		args.hasOwnProperty("scene") &&
 		state.scene === args.scene)
-			return callback(null, true);
-	
+		return callback(null, true);
+
 	return callback(null, false);
 });
