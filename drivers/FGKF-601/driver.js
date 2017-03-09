f86d8b5ccacb5d276ef3519d0ed96d788fe497ea
RobinBol <robinbolscher@gmail.com>
2017-03-09 16:39:37 +0100

    FGKF-601: add icon, use caseda's settings and flow card implementation, todo figure lock_mode out

--- drivers/FGKF-601/driver.js
+++ drivers/FGKF-601/driver.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
 
-// Documentation: http://Products.Z-WaveAlliance.org/ProductManual/File?folder=&filename=Manuals/2120/FGKF-601-EN-T-v1.0_30.11.2016.pdf
+// Documentation: http://products.z-wavealliance.org/ProductManual/File?folder=&filename=Manuals/2064/FGKF-601-EN-T-v1.0_30.11.2016.pdf
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	debug: true,
@@ -20,194 +20,175 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 	},
 	settings: {
-		1: {
-			index: 1,
-			size: 2,
-		},
-		2: {
-			index: 2,
-			size: 2,
-		},
-		3: {
+		sequence_1: {
 			index: 3,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		4: {
+		sequence_2: {
 			index: 4,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		5: {
+		sequence_3: {
 			index: 5,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		6: {
+		sequence_4: {
 			index: 6,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		7: {
+		sequence_5: {
 			index: 7,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		8: {
+		sequence_6: {
 			index: 8,
 			size: 2,
+			parser: newValue => parseSequence(newValue),
+			signed: false,
 		},
-		9: {
+		sequence_timeout: {
 			index: 9,
 			size: 1,
+			parser: newValue => new Buffer([newValue * 10]),
 		},
-		10: {
+		group_operating_mode: {
 			index: 10,
 			size: 1,
 		},
-		11: {
+		value_group_square: {
 			index: 11,
 			size: 2,
+			signed: false,
 		},
-		12: {
+		value_group_circle: {
 			index: 12,
 			size: 2,
+			signed: false,
 		},
-		13: {
+		value_group_cross: {
 			index: 13,
 			size: 2,
+			signed: false,
 		},
-		14: {
+		value_group_triangle: {
 			index: 14,
 			size: 2,
+			signed: false,
 		},
-		15: {
+		value_group_minus: {
 			index: 15,
 			size: 2,
+			signed: false,
 		},
-		16: {
+		value_group_plus: {
 			index: 16,
 			size: 2,
+			signed: false,
 		},
-		17: {
-			index: 17,
-			size: 1,
-		},
-		18: {
-			index: 18,
-			size: 1,
-		},
-		19: {
-			index: 19,
-			size: 1,
-		},
-		21: {
+		square_scenes: {
 			index: 21,
 			size: 1,
 		},
-		22: {
+		circle_scenes: {
 			index: 22,
 			size: 1,
 		},
-		23: {
+		cross_scenes: {
 			index: 23,
 			size: 1,
 		},
-		24: {
+		triangle_scenes: {
 			index: 24,
 			size: 1,
 		},
-		25: {
+		minus_scenes: {
 			index: 25,
 			size: 1,
 		},
-		26: {
+		plus_scenes: {
 			index: 26,
 			size: 1,
-		},
-		29: {
-			index: 29,
-			size: 2,
 		},
 	},
 });
 
-module.exports.on('initNode', (token) => {
+module.exports.on('initNode', token => {
 	const node = module.exports.nodes[token];
 
-	if (node) {
+	if (node && typeof node.instance.CommandClass.COMMAND_CLASS_CENTRAL_SCENE !== 'undefined') {
+
 		node.instance.CommandClass.COMMAND_CLASS_CENTRAL_SCENE.on('report', (command, report) => {
 			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
+
 				if (report &&
+					report.hasOwnProperty('Scene Number') &&
 					report.hasOwnProperty('Properties1') &&
 					report.Properties1.hasOwnProperty('Key Attributes')) {
 
-					const button_value = { scene: report.Properties1['Key Attributes'] };
-
-					if (report['Scene Number'] === 1)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_square', null, button_value, node.device_data);
-					if (report['Scene Number'] === 2)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_circle', null, button_value, node.device_data);
-					if (report['Scene Number'] === 3)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_cross', null, button_value, node.device_data);
-					if (report['Scene Number'] === 4)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_triangle', null, button_value, node.device_data);
-					if (report['Scene Number'] === 5)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_minus', null, button_value, node.device_data);
-					if (report['Scene Number'] === 6)
-						Homey.manager('flow').triggerDevice('FGKF-601_button_press_plus', null, button_value, node.device_data);
-
+					if (report['Scene Number'] <= 6) {
+						Homey.manager('flow').triggerDevice('FGKF-601-scene', null, {
+							button: report['Scene Number'].toString(),
+							scene: report.Properties1['Key Attributes'],
+						}, node.device_data);
+					} else {
+						Homey.manager('flow').triggerDevice('FGKF-601-sequence', null, {
+							sequence: report['Scene Number'].toString(),
+						}, node.device_data);
+					}
 				}
 			}
 		});
 	}
 });
 
-Homey.manager('flow').on('trigger.FGKF-601_button_press_square', (callback, args, state) => {
-	if (state && args &&
+Homey.manager('flow').on('trigger.FGKF-601-scene', (callback, args, state) => {
+	if (state &&
+		state.hasOwnProperty('button') &&
 		state.hasOwnProperty('scene') &&
+		args.hasOwnProperty('button') &&
 		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
-
+		state.button === args.button &&
+		state.scene === args.scene) {
+		return callback(null, true);
+	}
 	return callback(null, false);
 });
 
-Homey.manager('flow').on('trigger.FGKF-601_button_press_circle', (callback, args, state) => {
-	if (state && args &&
-		state.hasOwnProperty('scene') &&
-		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
-
+Homey.manager('flow').on('trigger.FGKF-601-sequence', (callback, args, state) => {
+	if (state &&
+		state.hasOwnProperty('sequence') &&
+		args.hasOwnProperty('sequence') &&
+		state.sequence === args.sequence) {
+		return callback(null, true);
+	}
 	return callback(null, false);
 });
 
-Homey.manager('flow').on('trigger.FGKF-601_button_press_cross', (callback, args, state) => {
-	if (state && args &&
-		state.hasOwnProperty('scene') &&
-		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
+function parseSequence(sequence) {
+	// if gesture is disabled return 0 as value
+	if (sequence === 0) return new Buffer([0, 0]);
 
-	return callback(null, false);
-});
+	// split sequence into individual buttons
+	const buttons = sequence.split(';').map(Number);
 
-Homey.manager('flow').on('trigger.FGKF-601_button_press_triangle', (callback, args, state) => {
-	if (state && args &&
-		state.hasOwnProperty('scene') &&
-		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
+	// Parse the buttons to their corresponding value
+	let parsing = buttons[0] + 8 * buttons[1];
+	if (buttons[2]) parsing += 64 * buttons[2];
+	if (buttons[3]) parsing += 512 * buttons[3];
+	if (buttons[4]) parsing += 4096 * buttons[4];
 
-	return callback(null, false);
-});
-
-Homey.manager('flow').on('trigger.FGKF-601_button_press_minus', (callback, args, state) => {
-	if (state && args &&
-		state.hasOwnProperty('scene') &&
-		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
-
-	return callback(null, false);
-});
-
-Homey.manager('flow').on('trigger.FGKF-601_button_press_plus', (callback, args, state) => {
-	if (state && args &&
-		state.hasOwnProperty('scene') &&
-		args.hasOwnProperty('scene') &&
-		state.scene === args.scene) { return callback(null, true); }
-
-	return callback(null, false);
-});
+	// return parsed buffer value
+	const parsedSequence = new Buffer(2);
+	parsedSequence.writeUIntBE(parsing, 0, 2);
+	return parsedSequence;
+}
