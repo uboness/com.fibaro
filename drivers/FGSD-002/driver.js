dc2998e1476299e0a34ece08af530cd80fafb75d
R Mengers <caseda08@hotmail.com>
2017-04-24 15:11:22 +0200

    FGSD-002: add notification CC (#137)

--- drivers/FGSD-002/driver.js
+++ drivers/FGSD-002/driver.js
@@ -7,41 +7,76 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		alarm_smoke: [{
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_SENSOR_ALARM',
-			command_get: 'SENSOR_ALARM_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Smoke Alarm',
-			}),
-			command_report: 'SENSOR_ALARM_REPORT',
-			command_report_parser: report => {
-				if (report['Sensor Type'] !== 'Smoke Alarm') return null;
-
-				return report['Sensor State'] === 'alarm';
+		alarm_smoke: [
+			{
+				getOnWakeUp: true,
+				command_class: 'COMMAND_CLASS_SENSOR_ALARM',
+				command_get: 'SENSOR_ALARM_GET',
+				command_get_parser: node => {
+					if (node && typeof node.state.alarm_smoke === 'undefined') {
+						modules.export.realtime(node.device_data, 'alarm_smoke', false);
+					}
+					return {
+						'Sensor Type': 'Smoke Alarm',
+					}
+				},
+				command_report: 'SENSOR_ALARM_REPORT',
+				command_report_parser: report => {
+					if (report['Sensor Type'] !== 'Smoke Alarm') return null;
+					return report['Sensor State'] === 'alarm';
+				},
 			},
-		}, {
-			command_class: 'COMMAND_CLASS_BASIC',
-			command_report: 'BASIC_SET',
-			command_report_parser: report => {
-				if (report && report.hasOwnProperty('Value')) return report.Value === 255;
-				return null;
+			{
+				command_class: 'COMMAND_CLASS_NOTIFICATION',
+				command_report: 'NOTIFICATION_REPORT',
+				command_report_parser: report => {
+					if (report && report['Notification Type'] === 'Smoke') {
+						if (report['Event'] === 1 || report['Event'] === 2 || report['Event'] === 3) return true;
+						return false
+					}
+					return null;
+				},
 			},
-		}],
-		alarm_heat: {
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_SENSOR_ALARM',
-			command_get: 'SENSOR_ALARM_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Heat Alarm',
-			}),
-			command_report: 'SENSOR_ALARM_REPORT',
-			command_report_parser: report => {
-				if (report['Sensor Type'] !== 'Heat Alarm') return null;
-
-				return report['Sensor State'] === 'alarm';
+			{
+				command_class: 'COMMAND_CLASS_BASIC',
+				command_report: 'BASIC_SET',
+				command_report_parser: report => {
+					if (report && report.hasOwnProperty('Value')) return report.Value === 255;
+					return null;
+				},
 			},
-		},
+		],
+		alarm_heat: [
+			{
+				getOnWakeUp: true,
+				command_class: 'COMMAND_CLASS_SENSOR_ALARM',
+				command_get: 'SENSOR_ALARM_GET',
+				command_get_parser: node => {
+					if (node && typeof node.state.alarm_heat === 'undefined') {
+						modules.export.realtime(node.device_data, 'alarm_heat', false);
+					}
+					return {
+						'Sensor Type': 'Heat Alarm',
+					}
+				},
+				command_report: 'SENSOR_ALARM_REPORT',
+				command_report_parser: report => {
+					if (report['Sensor Type'] !== 'Heat Alarm') return null;
+					return report['Sensor State'] === 'alarm';
+				},
+			},
+			{
+				command_class: 'COMMAND_CLASS_NOTIFICATION',
+				command_report: 'NOTIFICATION_REPORT',
+				command_report_parser: report => {
+					if (report && report['Notification Type'] === 'Heat') {
+						if (report['Event'] === 1 || report['Event'] === 2 || report['Event'] === 3 || report['Event'] === 4 || report['Event'] === 7) return true;
+						return false
+					}
+					return null;
+				},
+			},
+		],
 		measure_temperature: {
 			getOnWakeUp: true,
 			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
--- drivers/FGSS-001/driver.js
+++ drivers/FGSS-001/driver.js
@@ -7,34 +7,47 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		alarm_smoke: [{
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_SENSOR_ALARM',
-			command_get: 'SENSOR_ALARM_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Smoke Alarm',
-			}),
-			command_report: 'SENSOR_ALARM_REPORT',
-			command_report_parser: report => {
-				if (report['Sensor Type'] !== 'Smoke Alarm') return null;
+		alarm_smoke: [
+			{
+				getOnWakeUp: true,
+				command_class: 'COMMAND_CLASS_SENSOR_ALARM',
+				command_get: 'SENSOR_ALARM_GET',
+				command_get_parser: node => {
+					if (node && typeof node.state.alarm_smoke === 'undefined') {
+						modules.export.realtime(node.device_data, 'alarm_smoke', false);
+					}
+					return {
+						'Sensor Type': 'Smoke Alarm',
+					}
+				},
+				command_report: 'SENSOR_ALARM_REPORT',
+				command_report_parser: report => {
+					if (report['Sensor Type'] !== 'Smoke Alarm') return null;
 
-				return report['Sensor State'] === 'alarm';
+					return report['Sensor State'] === 'alarm';
+				},
 			},
-		}, {
-			command_class: 'COMMAND_CLASS_BASIC',
-			command_report: 'BASIC_SET',
-			command_report_parser: report => {
-				if (report && report.hasOwnProperty('Value')) return report.Value === 255;
-				return null;
-			},
-		}],
+			{
+				command_class: 'COMMAND_CLASS_BASIC',
+				command_report: 'BASIC_SET',
+				command_report_parser: report => {
+					if (report && report.hasOwnProperty('Value')) return report.Value === 255;
+					return null;
+				},
+			}
+		],
 		alarm_heat: {
 			getOnWakeUp: true,
 			command_class: 'COMMAND_CLASS_SENSOR_ALARM',
 			command_get: 'SENSOR_ALARM_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Heat Alarm',
-			}),
+			command_get_parser: node => {
+				if (node && typeof node.state.alarm_heat === 'undefined') {
+					modules.export.realtime(node.device_data, 'alarm_heat', false);
+				}
+				return {
+					'Sensor Type': 'Heat Alarm',
+				}
+			},
 			command_report: 'SENSOR_ALARM_REPORT',
 			command_report_parser: report => {
 				if (report['Sensor Type'] !== 'Heat Alarm') return null;
