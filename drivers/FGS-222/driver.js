281232ffebffc79cabbe6e7286bf99bba310df49
R Mengers <caseda08@hotmail.com>
2017-05-01 12:55:39 +0200

    FGS-222: setting size fix
    gotten the parameters from the device:
    parameter 4:
    Received application command for COMMAND_CLASS_CONFIGURATION, data: 0x0604020000
    parameter 5:
    Received application command for COMMAND_CLASS_CONFIGURATION, data: 0x0605020000

--- drivers/FGS-222/driver.js
+++ drivers/FGS-222/driver.js
@@ -13,7 +13,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			command_get: 'SWITCH_BINARY_GET',
 			command_set: 'SWITCH_BINARY_SET',
 			command_set_parser: value => ({
-				'Switch Value': (value > 0) ? 255 : 0,
+				'Switch Value': (value) ? 'on/enable' : 'off/disable',
 			}),
 			command_report: 'SWITCH_BINARY_REPORT',
 			command_report_parser: report => report.Value === 'on/enable',
@@ -26,11 +26,13 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		auto_off_relay_1: {
 			index: 4,
-			size: 4, //TODO is this 4? no documentation
+			size: 2,
+			signed: false,
 		},
 		auto_off_relay_2: {
 			index: 5,
-			size: 4, //TODO is this 4? no documentation
+			size: 2,
+			signed: false,
 		},
 		switch_type: {
 			index: 14,
