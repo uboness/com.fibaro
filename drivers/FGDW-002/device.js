b08965249e30f3377e34f0fd9550c86400e0143b
Matthew Waanders <matthew@athom.nl>
2018-04-03 10:31:00 +0200

    2nd generation motion sensor to settings extension, small refactoring for motion sensors

--- drivers/FGDW-002/device.js
+++ drivers/FGDW-002/device.js
@@ -5,65 +5,10 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroDoorSensorTwo extends ZwaveDevice {
 
 	onMeshInit() {
-		this.registerCapability('alarm_contact', 'NOTIFICATION', {
-			getOpts: {
-				getOnOnline: true,
-			},
-		});
-		this.registerCapability('alarm_tamper', 'NOTIFICATION', {
-			getOpts: {
-				getOnOnline: true,
-			},
-		});
+		this.registerCapability('alarm_contact', 'NOTIFICATION');
+		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 		this.registerCapability('measure_battery', 'BATTERY');
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
-
-		/*
-		=====================================================
-		General setting registration
-		=====================================================
-		*/
-		this.registerSetting('door_window_state');
-		this.registerSetting('association_group_2_triggers');
-		this.registerSetting('command_open');
-		this.registerSetting('command_close');
-		this.registerSetting('command_open_delay');
-		this.registerSetting('command_close_delay');
-
-		/*
-		=====================================================
-		LED indication setting registration
-		=====================================================
-		*/
-		this.registerSetting('led_indications');
-
-		/*
-		=====================================================
-		Tamper group setting registration
-		=====================================================
-		*/
-		this.registerSetting('tamper_cancellation_delay');
-		this.registerSetting('tamper_cancellation');
-
-		/*
-		=====================================================
-		Temperature group setting registration
-		=====================================================
-		*/
-		this.registerSetting('temperature_report_threshold');
-		this.registerSetting('temperature_measuring_interval');
-		this.registerSetting('temperature_report_interval');
-		this.registerSetting('temperature_offset');
-		this.registerSetting('temperature_alarm_reports');
-		this.registerSetting('temperature_alarm_high');
-		this.registerSetting('temperature_alarm_low');
-
-		/*
-		=====================================================
-		Z-Wave association group setting registration
-		=====================================================
-		*/
-		this.registerSetting('secure_mode_associations');
 	}
 
 }
