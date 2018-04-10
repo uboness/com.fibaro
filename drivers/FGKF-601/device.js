09bc9e87cd88945d942fb3c88c2003a363140a50
Matthew Waanders <matthew@athom.nl>
2018-04-10 16:30:04 +0200

    finished implementation, flows added

--- drivers/FGKF-601/device.js
+++ drivers/FGKF-601/device.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class FibaroKeyfob extends ZwaveDevice {
@@ -7,7 +8,11 @@ class FibaroKeyfob extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY');
 
-		// Parsing of sequences before sending to Keyfob
+		// Registering flows
+        this._sceneFlowTrigger = new Homey.FlowCardTriggerDevice('FGKF-601-scene').registerRunListener(this._sceneRunListener).register();
+        this._sequenceFlowTrigger = new Homey.FlowCardTriggerDevice('FGKF-601-sequence').registerRunListener(this._sequenceRunListener).register();
+
+        // Parsing of sequences before sending to Keyfob
 		this.registerSetting('sequence_1', (newValue) => {
 			return this.sequenceParser(newValue);
 		});
@@ -26,7 +31,53 @@ class FibaroKeyfob extends ZwaveDevice {
         this.registerSetting('sequence_6', (newValue) => {
             return this.sequenceParser(newValue);
         });
+
+        if (this.node && typeof this.node.CommandClass.COMMAND_CLASS_CENTRAL_SCENE !== 'undefined') {
+            this.node.CommandClass.COMMAND_CLASS_CENTRAL_SCENE.on('report', (command, report) => {
+
+                this.log('Button(s) pressed');
+
+                if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
+                    if (report &&
+                        report.hasOwnProperty('Scene Number') &&
+                        report.hasOwnProperty('Properties1') &&
+                        report.Properties1.hasOwnProperty('Key Attributes')) {
+
+                        if (report['Scene Number'] <= 6) {
+                            this.log(`Singular button press. Button: ${report['Scene Number'].toString()}, scene: ${report.Properties1['Key Attributes']}`);
+                            this._sceneFlowTrigger.trigger(this, null, {button: report['Scene Number'].toString(), scene: report.Properties1['Key Attributes']});
+                        } else {
+                            this.log(`Sequence of buttons pressed. Sequence: ${report['Scene Number'].toString()}`);
+                            this._sequenceFlowTrigger.trigger(this, null, {sequence: report['Scene Number'].toString()});
+                        }
+                    }
+                }
+            })
+        }
 	}
+
+    _sceneRunListener(args, state, callback) {
+        if (state &&
+            state.hasOwnProperty('button') &&
+            state.hasOwnProperty('scene') &&
+            args.hasOwnProperty('button') &&
+            args.hasOwnProperty('scene') &&
+            state.button === args.button &&
+            state.scene === args.scene) {
+            return callback(null, true);
+        }
+        return callback(null, false);
+    }
+
+	_sequenceRunListener(args, state, callback) {
+        if (state &&
+            state.hasOwnProperty('sequence') &&
+            args.hasOwnProperty('sequence') &&
+            state.sequence === args.sequence) {
+            return callback(null, true);
+        }
+        return callback(null, false);
+    }
 
 	sequenceParser(sequence) {
         // if gesture is disabled return 0 as value
