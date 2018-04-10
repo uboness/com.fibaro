d6a67d2ae36fe69b33a97fe0f45ff26fff9c4c3c
Matthew Waanders <matthew@athom.nl>
2018-04-10 13:30:46 +0200

    added getOnStart for on/off capability

--- drivers/FGWPE-101/device.js
+++ drivers/FGWPE-101/device.js
@@ -5,7 +5,11 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroWallPlug extends ZwaveDevice {
 
 	async onMeshInit() {
-		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this.registerCapability('onoff', 'SWITCH_BINARY', {
+			getOpts: {
+				getOnStart: true
+			}
+		});
 		this.registerCapability('measure_power', 'SENSOR_MULTILEVEL');
 		this.registerCapability('meter_power', 'METER');
 	}
