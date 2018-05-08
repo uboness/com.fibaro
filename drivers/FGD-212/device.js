8e6cd01c543c95f1c2b60d4ea313e0c60af5ff88
Matthew Waanders <matthew@athom.nl>
2018-05-08 16:01:20 +0200

    first work on FGD-212

---
+++ drivers/FGD-212/device.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class FibaroDimmerTwoDevice extends ZwaveDevice {
+	
+	onMeshInit() {
+
+	}
+	
+}
+
+module.exports = FibaroDimmerTwoDevice;
