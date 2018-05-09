a4516d11e9727f9238bbf50141c6576543e34924
Matthew Waanders <matthew@athom.nl>
2018-05-09 13:39:47 +0200

    setting and capability registration. divided settings into groups

--- drivers/FGD-212/device.js
+++ drivers/FGD-212/device.js
@@ -5,8 +5,14 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroDimmerTwoDevice extends ZwaveDevice {
 	
 	onMeshInit() {
+		this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
+        this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+        this.registerCapability('measure_power', 'SENSOR_MULTILEVEL');
+        this.registerCapability('meter_power', 'METER');
 
-	}
+        this.registerSetting('force_no_dim', value => new Buffer([value ? 2: 0]));
+        this.registerSetting('kwh_report', value => new Buffer([value * 100]));
+    }
 	
 }
 
