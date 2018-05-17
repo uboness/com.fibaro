e84317da7dcd5cbb0f6a50a070e99de358bb84fc
Matthew Waanders <matthew@athom.nl>
2018-05-17 09:39:25 +0200

    local branch Z-Wave fixes, implementation for FGR-221

---
+++ drivers/FGR-221/device.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class FibaroRollerShutterDevice extends ZwaveDevice {
+	
+	onMeshInit() {
+		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+	}
+	
+}
+
+module.exports = FibaroRollerShutterDevice;
