1b54397b5fb4beedcc4bb20b34af6c2504662db0
Matthew Waanders <matthew@athom.nl>
2018-05-17 12:01:03 +0200

    fixed timing issue, fixed getting old Value

--- drivers/FGKF-601/device.js
+++ drivers/FGKF-601/device.js
@@ -13,30 +13,30 @@ class FibaroKeyfob extends ZwaveDevice {
         this._sequenceFlowTrigger = new Homey.FlowCardTriggerDevice('FGKF-601-sequence').registerRunListener(this._sequenceRunListener).register();
 
         // Parsing of sequences before sending to Keyfob
-        this.registerSetting('lock_timeout', async (oldValue, newValue) => {
+        this.registerSetting('lock_timeout', async (newValue) => {
             try {
                 let requestTimeout = setTimeout(() => {
-                    Promise.reject('Device Sleeping');
-                }, 500);
+                    Promise.reject('device_asleep');
+                }, 5000);
                 await this._enableLockMode({lock_timeout: newValue});
                 clearTimeout(requestTimeout);
                 return newValue;
             } catch (err) {
                 this.error(err);
-                return oldValue;
+                return this.getSetting('lock_timeout');
             }
         });
-        this.registerSetting('sequence_lock', async (oldValue, newValue) => {
+        this.registerSetting('sequence_lock', async (newValue) => {
             try {
                 let requestTimeout = setTimeout(() => {
-                    Promise.reject('Device Sleeping');
-                }, 500);
+                    Promise.reject('device_asleep');
+                }, 5000);
                 await this._enableLockMode({sequence_lock: newValue});
                 clearTimeout(requestTimeout);
                 return this.sequenceParser(newValue);
             } catch (err) {
                 this.error(err);
-                return oldValue;
+                return this.getSetting('sequence_lock');
             }
         });
 		this.registerSetting('sequence_1', (newValue) => {
