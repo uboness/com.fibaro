3c18fc1f2fb1058a0b8d2367ecc2edf7ae5468c4
Matthew Waanders <matthew@athom.nl>
2018-06-12 12:14:59 +0200

    eslint --fix

--- drivers/FGS-221/device.js
+++ drivers/FGS-221/device.js
@@ -3,17 +3,17 @@
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class FibaroDoubleSwitchDevice extends ZwaveDevice {
-	
+
 	onMeshInit() {
-		if(!this.node.isMultiChannelNode) {
-            this.registerCapability('onoff', 'SWITCH_BINARY');
-        } else {
-            this.registerCapability('onoff', 'SWITCH_BINARY', {
-            	multiChannelNodeId: 2
+		if (!this.node.isMultiChannelNode) {
+			this.registerCapability('onoff', 'SWITCH_BINARY');
+		} else {
+			this.registerCapability('onoff', 'SWITCH_BINARY', {
+            	multiChannelNodeId: 2,
 			});
-        }
+		}
 	}
-	
+
 }
 
 module.exports = FibaroDoubleSwitchDevice;
