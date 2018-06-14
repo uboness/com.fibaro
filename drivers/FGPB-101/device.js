16125802ba05e536ffe0be5f34310c52b748e6f6
Matthew Waanders <matthew@athom.nl>
2018-06-14 12:14:19 +0200

    eslint --fxi, removed settings file, removed double spaces

--- drivers/FGPB-101/device.js
+++ drivers/FGPB-101/device.js
@@ -4,15 +4,15 @@ const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class Button extends ZwaveDevice {
-	
+
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY');
-		this._onButtonTrigger = new Homey.FlowCardTriggerDevice("FGPB-101").register().registerRunListener((args, state, callback) => {
-			if(state && args &&
+		this._onButtonTrigger = new Homey.FlowCardTriggerDevice('FGPB-101').register().registerRunListener((args, state, callback) => {
+			if (state && args &&
 				state.hasOwnProperty('scene') &&
 				args.hasOwnProperty('scene')) {
-					return callback(null, state.scene === args.scene);
-            }
+				return callback(null, state.scene === args.scene);
+			}
 		});
 
 		this.node.CommandClass.COMMAND_CLASS_CENTRAL_SCENE.on('report', (command, report) => {
@@ -21,22 +21,22 @@ class Button extends ZwaveDevice {
 			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
 				if (report &&
 					report.Properties1.hasOwnProperty('Key Attributes')) {
-					const buttonValue = {scene: report.Properties1['Key Attributes']};
+					const buttonValue = { scene: report.Properties1['Key Attributes'] };
 					if (buttonValue.scene === 'Key Released') {
 						if (debouncer === 0) {
-                            this._onButtonTrigger.trigger(this, null, buttonValue);
+							this._onButtonTrigger.trigger(this, null, buttonValue);
 
 							debouncer++;
 							setTimeout(() => debouncer = 0, 2000);
 						}
 					} else {
-                        this._onButtonTrigger.trigger(this, null, buttonValue);
-                    }
+						this._onButtonTrigger.trigger(this, null, buttonValue);
+					}
 				}
 			}
 		});
 	}
-	
+
 }
 
 module.exports = Button;
