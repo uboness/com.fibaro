702aed5b66c578b72e8de63b27d19a9a662cb2c8
Matthew Waanders <matthew@athom.nl>
2018-06-14 10:43:03 +0200

    small fixes, capitalisation changes, meshdriver update, rebuilt app

--- drivers/FGT-001/device.js
+++ drivers/FGT-001/device.js
@@ -7,17 +7,20 @@ class RadiatorThermostat extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY', {
 			getOpts: {
-				pollInterval: 'poll_interval_battery'
-			}
+				pollInterval: 'poll_interval_battery',
+                pollMultiplication: 1000
+            }
 		});
         this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
         	getOpts: {
-        		pollInterval: 'poll_interval_measure_temperature'
+        		pollInterval: 'poll_interval_measure_temperature',
+				pollMultiplication: 1000
 			}
 		});
         this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT',  {
             getOpts: {
-                pollInterval: 'poll_interval_target_temperature'
+                pollInterval: 'poll_interval_target_temperature',
+                pollMultiplication: 1000
             }
         });
     }
