520c89369c25bd6de9de43ed80083262eaca99a5
Matthew Waanders <matthew@athom.nl>
2018-06-14 16:23:05 +0200

    Small fix to FGS-221

--- drivers/FGS-221/device.js
+++ drivers/FGS-221/device.js
@@ -5,13 +5,7 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class FibaroDoubleSwitchDevice extends ZwaveDevice {
 
 	onMeshInit() {
-		if (!this.node.isMultiChannelNode) {
 			this.registerCapability('onoff', 'SWITCH_BINARY');
-		} else {
-			this.registerCapability('onoff', 'SWITCH_BINARY', {
-            	multiChannelNodeId: 2,
-			});
-		}
 	}
 
 }
