fb89f7ca49ac94e0cbdac42ae1a1bbd8fd64fdc9
Matthew Waanders <matthew@athom.nl>
2018-06-14 11:06:30 +0200

    re-add of FGCD-001

---
+++ drivers/FGCD-001/device.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class FibaroCODetectorDevice extends ZwaveDevice {
+
+	onMeshInit() {
+		this.registerCapability('alarm_co', 'NOTIFICATION');
+		this.registerCapability('alarm_heat', 'NOTIFICATION');
+
+		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+		this.registerCapability('measure_battery', 'BATTERY');
+	}
+
+}
+
+module.exports = FibaroCODetectorDevice;
