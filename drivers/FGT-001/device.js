f54fdfe65439dd2a4f1872ff89b5c5bfb70aa997
Matthew Waanders <matthew@athom.nl>
2018-06-14 10:44:46 +0200

    ESlint --fix

--- drivers/FGT-001/device.js
+++ drivers/FGT-001/device.js
@@ -3,28 +3,28 @@
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class RadiatorThermostat extends ZwaveDevice {
-	
+
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY', {
 			getOpts: {
 				pollInterval: 'poll_interval_battery',
-                pollMultiplication: 1000
-            }
+				pollMultiplication: 1000,
+			},
 		});
-        this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
         	getOpts: {
         		pollInterval: 'poll_interval_measure_temperature',
-				pollMultiplication: 1000
-			}
+				pollMultiplication: 1000,
+			},
 		});
-        this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT',  {
-            getOpts: {
-                pollInterval: 'poll_interval_target_temperature',
-                pollMultiplication: 1000
-            }
-        });
-    }
-	
+		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+			getOpts: {
+				pollInterval: 'poll_interval_target_temperature',
+				pollMultiplication: 1000,
+			},
+		});
+	}
+
 }
 
 module.exports = RadiatorThermostat;
