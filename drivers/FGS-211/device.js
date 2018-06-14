66300944432bc94a6430e84b96feb808f7cc741a
Matthew Waanders <matthew@athom.nl>
2018-06-14 13:09:13 +0200

    processed feedback

--- drivers/FGS-211/device.js
+++ drivers/FGS-211/device.js
@@ -3,11 +3,11 @@
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class FibaroRelaySwitchDevice extends ZwaveDevice {
-	
+
 	onMeshInit() {
 		this.registerCapability('onoff', 'SWITCH_BINARY');
 	}
-	
+
 }
 
 module.exports = FibaroRelaySwitchDevice;
