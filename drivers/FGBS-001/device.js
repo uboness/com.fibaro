d33cb892071714d9733afa0825ed9d073a69cc42
Matthew Waanders <matthew@athom.nl>
2018-07-18 10:55:11 +0200

    override for reportParsers

--- drivers/FGBS-001/device.js
+++ drivers/FGBS-001/device.js
@@ -68,11 +68,13 @@ class FibaroUniversalBinarySensor extends ZwaveDevice {
 		this.registerCapability('alarm_generic.contact1', 'SCENE_ACTIVATION', {
 			report: 'SCENE_ACTIVATION_SET',
 			reportParser: (report) => this._alarmGenericReportParser(report, 1),
+			reportParserOverride: true,
 		});
 		this.registerCapability('alarm_generic.contact2', 'SCENE_ACTIVATION', {
 			report: 'SCENE_ACTIVATION_SET',
 			reportParser: (report) => this._alarmGenericReportParser(report, 2),
-		});
+            reportParserOverride: true,
+        });
 
 		/*
     	=========================================================================
@@ -82,19 +84,23 @@ class FibaroUniversalBinarySensor extends ZwaveDevice {
 		this.registerCapability('measure_temperature.sensor1', 'SENSOR_MULTILEVEL', {
 			multiChannelNodeId: 3,
 			reportParser: (report) => this._temperatureReportParser(report, 1),
-		});
+            reportParserOverride: true,
+        });
 		this.registerCapability('measure_temperature.sensor2', 'SENSOR_MULTILEVEL', {
 			multiChannelNodeId: 4,
 			reportParser: (report) => this._temperatureReportParser(report, 2),
-		});
+            reportParserOverride: true,
+        });
 		this.registerCapability('measure_temperature.sensor3', 'SENSOR_MULTILEVEL', {
 			multiChannelNodeId: 5,
 			reportParser: (report) => this._temperatureReportParser(report, 3),
-		});
+            reportParserOverride: true,
+        });
 		this.registerCapability('measure_temperature.sensor4', 'SENSOR_MULTILEVEL', {
 			multiChannelNodeId: 6,
 			reportParser: (report) => this._temperatureReportParser(report, 4),
-		});
+            reportParserOverride: true,
+        });
 
 		this.registerSetting('12', (newValue) => new Buffer([Math.round(newValue / 16 * 255)]));
 	}
